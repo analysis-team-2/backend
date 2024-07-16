0cc5f2424870be46989efef730f9e42865cb87c6
KIM0212DH <godqhr803@naver.com>
2024-07-16 21:39:58 +0900

    dfdf

--- Team2_ml/ml/predict_amt.py
+++ Team2_ml/ml/predict_amt.py
@@ -3,17 +3,14 @@ import sys
 import pandas as pd
 from autogluon.tabular import TabularDataset, TabularPredictor
 import os
-import logging
+import warnings
 
 def main():
+    warnings.filterwarnings("ignore")
 
-# AutoGluon 로깅 비활성화
     logging.getLogger("autogluon").setLevel(logging.CRITICAL)
     logging.getLogger("autogluon.core").setLevel(logging.CRITICAL)
     logging.getLogger("autogluon.tabular").setLevel(logging.CRITICAL)
-
-
-
 
     if len(sys.argv) < 2:
         print(json.dumps({"status": "error", "message": "No input data provided"}, ensure_ascii=False))
@@ -22,10 +19,12 @@ def main():
     input_data = sys.argv[1]
 
     try:
-        data = json.loads(input_data)
-        print(data)
-        # JSON 데이터를 DataFrame으로 변환
-        df_predict = pd.DataFrame([data])  # 단일 딕셔너리를 데이터프레임으로 변환
+        # 따옴표로 감싸진 JSON 데이터를 디코딩
+        data = json.loads(input_data[1:-1].replace('\\"', '"'))
+
+        print(data)  # 디버깅 메시지
+
+        df_predict = pd.DataFrame(data)
         df_predict = df_predict.rename(columns={
             'store_avg_period': '운영점포평균영업기간',
             'shutdown_avg_period': '폐업점포평균영업기간',
@@ -33,32 +32,29 @@ def main():
         })
         df_predict = TabularDataset(df_predict)
 
-        # 모델 로드
         model_path = r"ml/ag-20240715_073451"
         if not os.path.exists(model_path):
             raise FileNotFoundError(f"모델 경로를 찾을 수 없습니다: {model_path}")
+
         predictor2 = TabularPredictor.load(model_path)
 
-        # 'amt' 열을 제외하고 예측
         y_pred = predictor2.predict(df_predict.drop(columns=['amt']))
 
-        # 예측 값을 float으로 변환
         predicted_value = float(y_pred.iloc[0])
 
-        # 결과를 JSON 형태로 반환
         result = {
             "status": "success",
-            "actual_value": 365433405,  # 실제값 (예시로 입력)
-            "predicted_value": predicted_value  # numpy.float32 -> float 변환
+            "actual_value": 365433405,
+            "predicted_value": predicted_value
         }
+        print(json.dumps(result, ensure_ascii=False))  # 디버깅 메시지
+
     except Exception as e:
         result = {
             "status": "error",
             "message": str(e)
         }
-
-    # 결과를 JSON 형태로 표준 출력
-    print(json.dumps(result, ensure_ascii=False))
+        print(json.dumps(result, ensure_ascii=False))  # 디버깅 메시지
 
 if __name__ == "__main__":
     main()
